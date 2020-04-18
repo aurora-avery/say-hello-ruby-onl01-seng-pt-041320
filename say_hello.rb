c88de3f829e984c2d5e25dc04ef01b56c56f0fec
Aurora Avery <aurora.avery@yahoo.com>
2020-04-18 03:38:56 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,4 +1,4 @@
 # Build your say_hello method here
-def say_hello(name, language="Ruby")
-  puts "Hello, #{name} #{language} programmer!"
+def say_hello(name= "Ruby")
+  puts "Hello, #{name} programmer!"
 end
